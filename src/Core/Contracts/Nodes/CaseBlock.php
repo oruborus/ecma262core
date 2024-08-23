179fbff7bfd9aaab6b7a5829082b05920a217321
[Action] Felix Jahn <felix.jahn@outlook.com>
2024-08-23 15:05:25 +0000

    CHORE: Automatic sync with https://github.com/oruborus/ecma262

--- src/Core/Contracts/Nodes/CaseBlock.php
+++ src/Core/Contracts/Nodes/CaseBlock.php
@@ -6,11 +6,12 @@ namespace Oru\EcmaScript\Core\Contracts\Nodes;
 
 use Oru\EcmaScript\Core\Contracts\Agent;
 use Oru\EcmaScript\Core\Contracts\Values\AbruptCompletion;
+use Oru\EcmaScript\Core\Contracts\Values\BooleanValue;
 use Oru\EcmaScript\Core\Contracts\Values\LanguageValue;
 use Oru\EcmaScript\Core\Contracts\Values\ListValue;
 use Oru\EcmaScript\Core\Contracts\Values\StringValue;
 
-interface CaseBlock extends BlockStatement
+interface CaseBlock extends Node
 {
     /**
      * @see https://262.ecma-international.org/12.0/#sec-runtime-semantics-caseblockevaluation
@@ -46,4 +47,26 @@ interface CaseBlock extends BlockStatement
      * @return ListValue<int, Declaration>
      */
     public function varScopedDeclarations(Agent $agent): ListValue;
+
+    /**
+     * @see https://tc39.es/ecma262/#sec-static-semantics-containsduplicatelabels
+     *
+     * @param ListValue<int, StringValue> $labelSet
+     */
+    public function containsDuplicateLabels(Agent $agent, ListValue $labelSet): BooleanValue;
+
+    /**
+     * @see https://tc39.es/ecma262/#sec-static-semantics-containsundefinedbreaktarget
+     *
+     * @param ListValue<int, StringValue> $labelSet
+     */
+    public function containsUndefinedBreakTarget(Agent $agent, ListValue $labelSet): BooleanValue;
+
+    /**
+     * @see https://tc39.es/ecma262/#sec-static-semantics-containsundefinedcontinuetarget
+     *
+     * @param ListValue<int, StringValue> $iterationSet
+     * @param ListValue<int, StringValue> $labelSet
+     */
+    public function containsUndefinedContinueTarget(Agent $agent, ListValue $iterationSet, ListValue $labelSet): BooleanValue;
 }
--- src/Core/Contracts/Nodes/Finally_.php
+++ src/Core/Contracts/Nodes/Finally_.php
@@ -4,6 +4,32 @@ declare(strict_types=1);
 
 namespace Oru\EcmaScript\Core\Contracts\Nodes;
 
-interface Finally_ extends BlockStatement
+use Oru\EcmaScript\Core\Contracts\Agent;
+use Oru\EcmaScript\Core\Contracts\Values\BooleanValue;
+use Oru\EcmaScript\Core\Contracts\Values\ListValue;
+use Oru\EcmaScript\Core\Contracts\Values\StringValue;
+
+interface Finally_ extends Node
 {
+    /**
+     * @see https://tc39.es/ecma262/#sec-static-semantics-containsduplicatelabels
+     *
+     * @param ListValue<int, StringValue> $labelSet
+     */
+    public function containsDuplicateLabels(Agent $agent, ListValue $labelSet): BooleanValue;
+
+    /**
+     * @see https://tc39.es/ecma262/#sec-static-semantics-containsundefinedbreaktarget
+     *
+     * @param ListValue<int, StringValue> $labelSet
+     */
+    public function containsUndefinedBreakTarget(Agent $agent, ListValue $labelSet): BooleanValue;
+
+    /**
+     * @see https://tc39.es/ecma262/#sec-static-semantics-containsundefinedcontinuetarget
+     *
+     * @param ListValue<int, StringValue> $iterationSet
+     * @param ListValue<int, StringValue> $labelSet
+     */
+    public function containsUndefinedContinueTarget(Agent $agent, ListValue $iterationSet, ListValue $labelSet): BooleanValue;
 }
