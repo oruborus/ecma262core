df81f5635ec8f14be971632c2cda86ca6d2159d7
[Action] Felix Jahn <felix.jahn@outlook.com>
2025-11-09 15:37:40 +0000

    CHORE: Automatic sync with https://github.com/oruborus/ecma262

--- src/Core/Contracts/Grammars/FunctionsAndClasses/Productions/ClassElement.php
+++ src/Core/Contracts/Grammars/FunctionsAndClasses/Productions/ClassElement.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace Oru\EcmaScript\Core\Contracts\Grammars\FunctionsAndClasses\Productions;
 
 use Oru\EcmaScript\Core\Contracts\RuntimeSemantics\HasPropertyDefinitionEvaluation;
+use Oru\EcmaScript\Core\Contracts\StaticSemantics\HasPropName;
 use Oru\EcmaScript\Core\Contracts\Values\AbruptCompletion;
 use Oru\EcmaScript\Core\Contracts\Values\BooleanValue;
 use Oru\EcmaScript\Core\Contracts\Values\ClassElementKind;
 use Oru\EcmaScript\Core\Contracts\Values\MethodDefinitionRecord;
 use Oru\EcmaScript\Core\Contracts\Values\ObjectValue;
 
-interface ClassElement extends ClassElementList, HasPropertyDefinitionEvaluation
+interface ClassElement extends ClassElementList, HasPropertyDefinitionEvaluation, HasPropName
 {
     /**
      * @see https://262.ecma-international.org/12.0/#sec-static-semantics-classelementkind
